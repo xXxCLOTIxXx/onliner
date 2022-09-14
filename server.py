316e138c3059ae7df1697ec88a5b5f5c6a97a27a
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2022-09-14 15:20:13 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -8,34 +8,12 @@ ip="192.168.0.108"
 host = "http://192.168.0.108:4567/"
 port="4567"
 app = Flask(__name__)
-players=list()
-
-def clear(id):
-    time.sleep(10)
-    with open('game.json','r') as file:
-        json_ = json.load(file)
-    del json_['messages'][id]
-    with open('game.json','w') as file:
-        json.dump(json_, file)
-
-def clear2(id):
-    players.append(id)
-    time.sleep(5)
-    try:
-        with open('game.json','r') as file:
-            json_ = json.load(file)
-        del json_['players'][id]
-        with open('game.json','w') as file:
-            json.dump(json_, file)
-    except KeyError:pass
-    players.remove(id)
 
 
 @app.route("/player-position")
 def position():
     with open(f'game.json','r') as file:
         json_ = json.load(file)
-    if request.args.get('id') not in players:Thread(target=clear2, args=(request.args.get('id'),)).start()
     return json_
 
 
@@ -59,7 +37,6 @@ def add_message():
     except:pass
     with open('game.json','w') as file:
         json.dump(json_, file)
-        Thread(target=clear, args=(req_json.get('uid'),)).start()
     return json_
 
 #app.run(debug=True, port=port, host=ip)
